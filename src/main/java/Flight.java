531505bea0e79b141ecd87f80b66877ddf059980
Claire Martin <claire.e.martin15@gmail.com>
2023-11-03 20:14:03 +0000

    sorted flight class

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,13 +1,88 @@
+import java.util.ArrayList;
+
 public class Flight {
 
     private String flightNumber;
     private DeptAirport deptAirport;
     private ArrivalAirport arrivalAirport;
     private String departureTime;
+    private Plane plane;
+    private Pilot pilot;
+    private ArrayList<CabinCrewMember> cabinCrewMembers;
+    private ArrayList<Passenger> passengers;
 
-//    *A plane
-//    A pilot
-//    A list of crew members
-//    a lost of passengers booked on the plane
+    public Flight(String flightNumber, DeptAirport deptAirport, ArrivalAirport arrivalAirport, String departureTime, Plane plane, Pilot pilot, ArrayList<CabinCrewMember> cabinCrewMembers, ArrayList<Passenger> passengers) {
+        this.flightNumber = flightNumber;
+        this.deptAirport = deptAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.departureTime = departureTime;
+        this.plane = plane;
+        this.pilot = pilot;
+        this.cabinCrewMembers = cabinCrewMembers;
+        this.passengers = passengers;
+    }
 
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public DeptAirport getDeptAirport() {
+        return deptAirport;
+    }
+
+    public void setDeptAirport(DeptAirport deptAirport) {
+        this.deptAirport = deptAirport;
+    }
+
+    public ArrivalAirport getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(ArrivalAirport arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
+
+    public Pilot getPilot() {
+        return pilot;
+    }
+
+    public void setPilot(Pilot pilot) {
+        this.pilot = pilot;
+    }
+
+    public ArrayList<CabinCrewMember> getCabinCrewMembers() {
+        return cabinCrewMembers;
+    }
+
+    public void setCabinCrewMembers(ArrayList<CabinCrewMember> cabinCrewMembers) {
+        this.cabinCrewMembers = cabinCrewMembers;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+    }
 }
